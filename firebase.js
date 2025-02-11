8cbc42f5864567f406e01e55bfc6b0b8ca0324d5
Franklyn101 <franklyninoma@gmail.com>
2025-02-11 17:43:16 +0100

    onboard

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: import.meta.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: import.meta.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBqf3-KG4OaPq-dPvFvoX8yHFOkqsO8HRM",
+  authDomain: "sincoin.firebaseapp.com",
+  projectId: "sincoin",
+  storageBucket: "sincoin.firebasestorage.app",
+  messagingSenderId: "266905404565",
+  appId: "1:266905404565:web:4733ee9fe0bd501312c79c",
+  measurementId: "G-T35TCRB3ZP"
 };
 
 // Initialize Firebase
