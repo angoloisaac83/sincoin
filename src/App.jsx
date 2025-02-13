5d50f593f9b460d80cf0056b84c60ae9c9535e8f
angoloisaac83 <110981606+angoloisaac83@users.noreply.github.com>
2025-02-13 07:17:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Login from './pages/login/page';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Mining from './pages/mining/page';
 import Layout from './pages/layout';
-import TicTacToe from './components/game';
+import TicTacToe from './components/Game.jsx';
 import Dashboard from './pages/dashboard/page';
 import Profile from './pages/profile/page';
 
