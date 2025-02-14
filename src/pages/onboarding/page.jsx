567310ff181a349db10243cd4fb2e7f890746b93
angoloisaac83 <110981606+angoloisaac83@users.noreply.github.com>
2025-02-14 09:47:52 +0100

    Update page.jsx

--- src/pages/onboarding/page.jsx
+++ src/pages/onboarding/page.jsx
@@ -7,7 +7,9 @@ const Onboarding = ()=>{
                 <img className='h-[50vh] object-cover w-full' src="https://i.pinimg.com/736x/c6/51/21/c651218a6c17efc7fb2cf237eb68bf9c.jpg" alt="" />
                 <div className='w-full py-[10px] h-[40vh] px-4 flex flex-col items-center gap-4 justify-center'>
                     <h1 className="text-4xl font-bold text-green-600 w-full">Begin your Mining journey with Sin Coin</h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione  mollitia error, iure quibusdam ab iusto! Mollitia id molestiae tempora amet.</p>
+                    <p>Register or Login</p>
+                    <p>Click on start mining</p>
+                    <p>Complete Daily tasks and wait for airdrop</p>
                     <Link to="/register"><button className='text-white bg-green-600 text-2xl rounded-full px-[30px] py-[8px]'>Get Mining</button></Link>
                 </div>
             </main>
